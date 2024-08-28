1658ce517d16f0889f8dca186299c5f16de8bfc9
Anjarul347 <skanjarulhossain347@gmail.com>
2024-08-28 15:05:45 +0530

    Scanner class error fixed

--- p3_MultiplicationTable.java
+++ p3_MultiplicationTable.java
@@ -7,12 +7,13 @@ import java.util.Scanner;
 public class p3_MultiplicationTable {
     public static void main(String[] args) {
         System.out.println("Ente a number to generate multiplication Table:-");
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        sc.close();
-        System.out.printf("The multiplication table of %d is given below:-\n",n);
-        for(int i=1;i<=10;i++){
-            System.out.printf("%d X %d = %d\n",n,i,(n*i));
+        try (Scanner sc = new Scanner(System.in)) {
+            int n=sc.nextInt();
+            sc.close();
+            System.out.printf("The multiplication table of %d is given below:-\n",n);
+            for(int i=1;i<=10;i++){
+                System.out.printf("%d X %d = %d\n",n,i,(n*i));
+            }
         }
     }
     
--- p4_HCF_LCM.java
+++ p4_HCF_LCM.java
@@ -7,10 +7,12 @@ import java.util.Scanner;
 public class p4_HCF_LCM {
     public static void main(String[] args) {
          System.out.println("Ente Two number to find its HCF And LCM:-");
-        Scanner sc=new Scanner(System.in);
-        int num1=sc.nextInt();
-        int num2 = sc.nextInt();
-        sc.close();
+         int num1;
+         int num2;
+        try (Scanner sc = new Scanner(System.in)) {
+            num1 = sc.nextInt();
+            num2 = sc.nextInt();
+        }
         int HCF=0,LCM;
         
         for(int i=1;i<=num1;i++){
